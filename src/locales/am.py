b8fe5bdecb1f3e0c79faf9ef753d6ccbcae61a80
otabek-rizakulov-cntxt <otabekbutcher@gmail.com>
2026-07-25 16:10:55 +0400

    feat(locale): add Amharic (አማርኛ) UI translation

---
+++ src/locales/am.py
@@ -0,0 +1,43 @@
+# Amharic — አማርኛ
+
+strings = {
+    "welcome": (
+        "የሱራና የአያት ቁጥር ላክልኝ፣ ለምሳሌ <b>2:255</b>፣ እኔም ከቅዱስ ቁርኣን ያንን አያት "
+        "እልክልሃለሁ።\n\n"
+        "እንደ <b>59:22-24</b> ያለ ክልል መላክም ትችላለህ፤ ያኔ የተጣመረ የድምፅ ቅርአት "
+        "ይደርስሃል።\n\n"
+        "ትዕዛዞች:\n"
+        "/index — የሁሉም ሱራዎች ዝርዝር\n"
+        "/random — በዘፈቀደ አያት\n"
+        "/language — ቋንቋ ቀይር\n"
+        "/about — ምንጮችና ምስጋና\n\n"
+        "በየትኛውም ውይይት ልትጠቀምበኝ ትችላለህ: <b>@QuranAyat_bot</b> ብለህ ጻፍ ከዚያም "
+        "የአያቱን ቁጥር አስገባ።"
+    ),
+    "about": (
+        "ይህ ቦት በቴሌግራም ላይ በብዙ ቋንቋዎች ቅዱስ ቁርኣንን ለማንበብ ያስችልሃል።\n\n"
+        "ትርጉሞቹ ከ tanzil.net የተወሰዱ ናቸው (የየራሱን እትምና ተርጓሚ ለማወቅ "
+        "ATTRIBUTIONS.md ተመልከት)። ድምፁ የሸይኽ መሕሙድ ኻሊል አል-ሑሰሪ ቅርአት ነው "
+        "(everyayah.com)። ተፍሲሩ ተፍሲር አል-ጀላለይን (altafsir.com) ሲሆን በእንግሊዝኛ "
+        "ይገኛል።\n\n"
+        "ቋንቋን በፈለከው ጊዜ በ /language ቀይር።\n"
+        "የምንጭ ኮድ: https://github.com/rahiel/BismillahBot"
+    ),
+    "ayah_not_found": "እንዲህ ያለ አያት አይገኝም!",
+    "range_too_large": "ክልሉ በጣም ትልቅ ነው፤ በአንድ ጊዜ ቢበዛ {n} አያቶች ጠይቅ።",
+    "choose_language": "ቋንቋህን ምረጥ:",
+    "language_set": "ቋንቋ ወደ {lang} ተቀይሯል።",
+    "tafsir_en_note": "\n\n(ተፍሲር በእንግሊዝኛ ብቻ ይገኛል።)",
+    "btn_translation": "ትርጉም",
+    "btn_tafsir": "ተፍሲር",
+    "btn_arabic": "ዓረብኛ",
+    "btn_audio": "ድምፅ",
+    "btn_previous": "ቀዳሚ",
+    "btn_random": "በዘፈቀደ",
+    "btn_next": "ቀጣይ",
+    "cmd_index": "የሁሉም ሱራዎች ዝርዝር",
+    "cmd_random": "በዘፈቀደ አያት",
+    "cmd_language": "ቋንቋ ቀይር",
+    "cmd_about": "ምንጮችና ምስጋና",
+    "quran_name": "ቁርኣን",
+}
